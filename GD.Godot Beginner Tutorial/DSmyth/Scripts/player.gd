3364edf9bb0bb716dd97b316fd470edfd163dae1
Declan Smyth <declan.smyth@hotmail.com>
2024-07-08 15:34:50 +0930

    Added custom Input Actions, Setup proper player anims, Created basic Slime enemy

--- GD.Godot Beginner Tutorial/DSmyth/Scripts/player.gd
+++ GD.Godot Beginner Tutorial/DSmyth/Scripts/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var animated_sprite = $AnimatedSprite2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
@@ -14,15 +15,34 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	# Get input direction (-1, 0, 1)
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	# Flip the sprite based on direction
+	if direction > 0:
+		animated_sprite.flip_h = false;
+	elif direction < 0:
+		animated_sprite.flip_h = true;
+	
+	# Play animations	
+	HandleAnimations(direction)
+	
+	# Apply movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func HandleAnimations(direction):
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("Idle")
+		else:
+			animated_sprite.play("Run")
+	else:
+		animated_sprite.play("Jumping")
